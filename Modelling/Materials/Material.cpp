d8be5c99fe7988fe9b3d5231143d3ec02e47c8f4
usuario <usuario@ubuntu-20.10-desktop-amd64>
2022-05-09 16:37:18 +0200

    now materiaal extends of serialization

--- Modelling/Materials/Material.cpp
+++ Modelling/Materials/Material.cpp
@@ -13,3 +13,76 @@ Material::Material() {
 void Material::toGPU(shared_ptr<QGLShaderProgram> program){
 }
 
+
+void Material::read (const QJsonObject &json)
+{
+    if (json.contains("ka") && json["ka"].isArray()) {
+        QJsonArray auxVec = json["ka"].toArray();
+        Ka[0] = auxVec[0].toDouble();
+        Ka[1] = auxVec[1].toDouble();
+        Ka[2] = auxVec[2].toDouble();
+    }
+    if (json.contains("kd") && json["kd"].isArray()) {
+        QJsonArray auxVec = json["kd"].toArray();
+        Kd[0] = auxVec[0].toDouble();
+        Kd[1] = auxVec[1].toDouble();
+        Kd[2] = auxVec[2].toDouble();
+    }
+    if (json.contains("ks") && json["ks"].isArray()) {
+        QJsonArray auxVec = json["ks"].toArray();
+        Ks[0] = auxVec[0].toDouble();
+        Ks[1] = auxVec[1].toDouble();
+        Ks[2] = auxVec[2].toDouble();
+    }
+    if (json.contains("kt") && json["kt"].isArray()) {
+        QJsonArray auxVec = json["kt"].toArray();
+        Kt[0] = auxVec[0].toDouble();
+        Kt[1] = auxVec[1].toDouble();
+        Kt[2] = auxVec[2].toDouble();
+    }
+    if (json.contains("shininess") && json["shininess"].isDouble())
+        shininess = json["shininess"].toDouble();
+    if (json.contains("opacity") && json["opacity"].isDouble())
+        opacity = json["opacity"].toDouble();
+    if (json.contains("nut") && json["nut"].isDouble())
+        nut = json["nut"].toDouble();
+}
+
+
+//! [1]
+void Material::write(QJsonObject &json) const
+{
+    QJsonArray auxArray;
+    auxArray.append(Ka[0]);auxArray.append(Ka[1]);auxArray.append(Ka[2]);
+    json["ka"] = auxArray;
+
+    QJsonArray auxArray2;
+    auxArray2.append(Kd[0]);auxArray2.append(Kd[1]);auxArray2.append(Kd[2]);
+    json["kd"] = auxArray2;
+
+    QJsonArray auxArray3;
+    auxArray3.append(Ks[0]);auxArray3.append(Ks[1]);auxArray3.append(Ks[2]);
+    json["ks"] = auxArray3;
+
+    QJsonArray auxArray4;
+    auxArray4.append(Kt[0]);auxArray4.append(Kt[1]);auxArray4.append(Kt[2]);
+    json["kt"] = auxArray4;
+
+    json["opacity"] = opacity;
+    json["shininess"] = shininess;
+    json["nut"] = nut;
+}
+
+//! [1]
+
+void Material::print(int indentation) const
+{
+    const QString indent(indentation * 2, ' ');
+
+    QTextStream(stdout) << indent << "Ka:\t" << Ka[0] << ", "<< Ka[1] << ", "<< Ka[2] << "\n";
+    QTextStream(stdout) << indent << "Kd:\t" << Kd[0] << ", "<< Kd[1] << ", "<< Kd[2] << "\n";
+    QTextStream(stdout) << indent << "Ks:\t" << Ks[0] << ", "<< Ks[1] << ", "<< Ks[2] << "\n";
+    QTextStream(stdout) << indent << "shininess:\t" << shininess<< "\n";
+    QTextStream(stdout) << indent << "opacity:\t" << opacity<< "\n";
+    QTextStream(stdout) << indent << "nut:\t" << nut<< "\n";
+}
--- Modelling/Materials/Material.h
+++ Modelling/Materials/Material.h
@@ -2,17 +2,31 @@
 
 #include <QGLShaderProgram>
 #include <memory>
+#include <DataSettings/Serializable.h>
+#include <vector>
 
 using namespace std;
 // TO DO: A canviar a la fase 1 de la practica 2
 // Classe que representa els materials d'un objecte
-class Material {
+class Material: public Serializable{
 
 
 public:
     Material();
 
     void toGPU(shared_ptr<QGLShaderProgram> program);
+    virtual void read (const QJsonObject &json);
+    virtual void write (QJsonObject &json) const;
+    virtual void print (int indentation) const;
+
+    vec3 Ka;
+    vec3 Kd;
+    vec3 Ks;
+    vec3 Kt;
+
+    float shininess;
+    float opacity;
+    float nut;
 
 };
 
